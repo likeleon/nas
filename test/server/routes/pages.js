13fdb14a98b951669d872eb5eb9bf2d058544b1f
likeleon <likeleon@gmail.com>
2014-01-17 01:47:23 +0900

    아이콘 추가

--- test/server/routes/pages.js
+++ test/server/routes/pages.js
@@ -8,7 +8,7 @@ describe('routes/pages', function() {
             .get('/')
             .end(function(err, res) {
                 res.should.have.status(200);
-                res.text.should.include('<table class="table table-hover table-condensed">');
+                res.text.should.include('<table id="files" class="table table-hover">');
                 done();
             })
         })
