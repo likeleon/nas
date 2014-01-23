1b3432ed3a836ffbffeb3d167b3386ce5e04a3b1
likeleon <likeleon@gmail.com>
2014-01-24 01:13:27 +0900

    angular-ui ui-router 추가

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,16 @@
 "use strict";
 
-window.nas = angular.module('nas', ['socketServices']);
+var nas = angular.module('nas', ['ui.router', 'socketServices']);
+
+nas.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider
+        .otherwise('/files');
+
+    $stateProvider
+        .state('files', {
+            url: '/files',
+            templateUrl: 'partials/main.html'
+        });
+}]);
+
+window.nas = nas;
