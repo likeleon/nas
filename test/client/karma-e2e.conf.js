349bd63b0b556d5a505a0d687bf5372a3387fcab
likeleon <likeleon@gmail.com>
2014-01-25 03:24:44 +0900

    클라이언트 end-to-end 테스트 기능 추가

---
+++ test/client/karma-e2e.conf.js
@@ -0,0 +1,31 @@
+module.exports = function(config) {
+    config.set({
+        basePath: '../../',
+
+        frameworks: ['ng-scenario'],
+
+        files: [
+            'public/js/*.js',
+            'public/js/**/*.js',
+            'test/client/e2e/*.js'
+        ],
+
+        autoWatch: false,
+
+        browsers: ['Chrome'],
+
+        singleRun: true,
+
+        urlRoot: '/nas_e2e/',
+
+        proxies: {
+            '/' : 'http://localhost:3000'
+        },
+
+        plugins: [
+            'karma-chrome-launcher',
+            'karma-jasmine',
+            'karma-ng-scenario'
+        ]
+    });
+};
